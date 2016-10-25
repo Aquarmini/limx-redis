65c15f3c89a616401414cb2f0cbd849a118808ee
李铭昕 <715557344@qq.com>
2016-10-25 10:11:48 +0800

    * 增加测试代码

--- tests/index.php
+++ tests/index.php
@@ -8,3 +8,15 @@
 // +----------------------------------------------------------------------
 // | Date: 2016/8/17 Time: 12:14
 // +----------------------------------------------------------------------
+namespace limx\tools;
+require __DIR__ . '/../src/MyRedis.php';
+
+$config['host'] = '';
+$config['auth'] = '';
+$config['database'] = 0;
+
+$redis = MyRedis::getInstance($config);
+$redis->select(1);
+print_r($redis->keys('*'));
+$redis->set('key1', 'val2');
+print_r($redis->get('key1'));
